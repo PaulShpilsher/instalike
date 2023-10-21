f7731af7bf456c380f87e8a25c194ed03933b293
Paul Shpilsher <paul.shpilsher@gmail.com>
2023-10-21 13:53:13 +0300

    add post id readed

--- pkg/posts/endpoint.go
+++ pkg/posts/endpoint.go
@@ -1,1 +1,18 @@
 package posts
+
+import (
+	"strconv"
+
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/log"
+)
+
+func getPostId(c *fiber.Ctx) (int, error) {
+	param := c.Params("postId")
+	value, err := strconv.Atoi(param)
+	if err != nil {
+		log.Errorf("bad post id param: %s. err: %v", param, err)
+		return 0, err
+	}
+	return value, nil
+}
