97cd00ef58f2db3fb58d1c7a6965fd86d8afb0ba
Paul Shpilsher <paul.shpilsher@gmail.com>
2023-10-21 14:32:23 +0300

    set explicitly return columns

--- pkg/posts/repository.go
+++ pkg/posts/repository.go
@@ -20,7 +20,7 @@ func (r *repository) CreatePost(userId int, contents string) (Post, error) {
 
 	post := Post{}
 
-	if err := r.DB.Get(&post, "INSERT INTO posts (user_id, contents) VALUES($1, $2) RETURNING *", userId, contents); err != nil {
+	if err := r.DB.Get(&post, "INSERT INTO posts (user_id, contents) VALUES($1, $2) RETURNING id, user_id, contents, like_count, created_at, updated_at", userId, contents); err != nil {
 		log.Printf("[DB ERROR]: %v", err)
 		return Post{}, err
 	}
