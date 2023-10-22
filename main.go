d1d61cd82861b8d25beb2f3b52d74538bd6b8c19
Paul Shpilsher <paul.shpilsher@gmail.com>
2023-10-22 17:44:38 +0300

    add declarative comments

--- main.go
+++ main.go
@@ -5,6 +5,17 @@ import (
 	"github.com/PaulShpilsher/instalike/pkg/webserver"
 )
 
+// @title InstaLike API
+// @version 2.0
+// @description This is a instagram-like server (instalike).
+// @termsOfService http://swagger.io/terms/
+
+// @license.name Apache 2.0
+// @license.url http://www.apache.org/licenses/LICENSE-2.0.html
+
+// @host localhost:3000
+// @BasePath /
+// @schemes http
 func main() {
 
 	config := config.LoadConfig()
